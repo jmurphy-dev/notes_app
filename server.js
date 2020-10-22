4442e566e1bb7d3ecb887ccc35f9cfee060fd199
Jack Murphy <signal.space@protonmail.com>
2020-10-22 17:59:29 -0500

    added router and notes page works

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // Dependencies
 // =============================================================
 const express = require("express");
-const path = require("path");
 
 // Sets up the Express App
 // =============================================================
@@ -12,19 +11,20 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Routes
-// =============================================================
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
-});
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-// Starts the server to begin listening
-// =============================================================
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
 app.listen(PORT, function () {
-  console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT: " + PORT);
 });
